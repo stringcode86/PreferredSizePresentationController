f5e8d788ddea52e440a8cd555320c3085ded6392
stringcode <michael@stringcode.co.uk>
2018-09-17 10:43:10 +0100

    Initial Commit

---
+++ PreferredSizePresentationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreferredSizePresentationController
+//
+//  Created by Michael Inger on 17/09/2018.
+//  Copyright © 2018 stringcode ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
